c6e6042c9afd01b5f5e42fa4712cd4d7c47a2ba9
Edric Li <edric.li@developertools.com>
2023-07-03 20:22:24 +0800

    update gitignore

--- Arsenal/Web/scripts/build.js
+++ Arsenal/Web/scripts/build.js
@@ -11,7 +11,7 @@ function main() {
   const distDir = path.resolve(__dirname, '../dist/assets');
   const targetDir = path.resolve(__dirname, '../../Resources');
   const files = fs.readdirSync(distDir);
-  fs.copySync(path.resolve(distDir, files[0]), path.resolve(targetDir, 'react.dist.js'));
+  fs.copySync(path.resolve(distDir, files[0]), path.resolve(targetDir, 'arsenal.core.js'));
 }
 
 main();
