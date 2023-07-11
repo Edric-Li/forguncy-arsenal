69a66fe3c959b2e085fc4b3d8881f27711cabcf9
Edric Li <edric.li@developertools.com>
2023-07-11 14:38:14 +0800

    Change the build script.

--- Arsenal/Web/scripts/build.js
+++ Arsenal/Web/scripts/build.js
@@ -11,7 +11,9 @@ function main() {
   const distDir = path.resolve(__dirname, '../dist/assets');
   const targetDir = path.resolve(__dirname, '../../Resources');
   const files = fs.readdirSync(distDir);
-  fs.copySync(path.resolve(distDir, files[0]), path.resolve(targetDir, 'arsenal.core.js'));
+
+  fs.copySync(path.resolve(distDir, files.find(item => item.endsWith('.js'))), path.resolve(targetDir, 'arsenal.core.js'));
+  fs.copySync(path.resolve(distDir, files.find(item => item.endsWith('.css'))), path.resolve(targetDir, 'arsenal.core.css'));
 }
 
 main();
