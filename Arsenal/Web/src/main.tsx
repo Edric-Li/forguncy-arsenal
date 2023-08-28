ee7b1648c26d6efcdf308d8e6e37a832a691f996
Edric.Li <7931743@qq.com>
2023-08-28 19:56:36 +0800

    Upgrade dependencies.

--- Arsenal/Web/src/main.tsx
+++ Arsenal/Web/src/main.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ReactCellTypeWrapper from './components/wrapper/celltype';
 import commandWrapper from './components/wrapper/command';
-// import VConsole from 'vconsole';
 import './index.css';
-
-// new VConsole({theme: 'dark'});
 
 function createReactComponent(cellType: CellType, componentName: ComponentName) {
   ReactDOM.createRoot(cellType.getContainer()[0]).render(
